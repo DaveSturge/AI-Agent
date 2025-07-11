5210882349fc73d4aa954bfab74046274490ebbb
DaveSturge <david-sturgi@hotmail.com>
2025-07-11 16:30:50 +0100

    Added AI function usage

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,20 @@
 import os
+from google import genai
+from google.genai import types
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
 
 def get_files_info(working_directory, directory = None):
     abs_working_dir = os.path.abspath(working_directory)
@@ -28,3 +44,4 @@ def get_files_info(working_directory, directory = None):
     joined_string = '\n'.join(output_list)
 
     return joined_string
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -17,11 +18,27 @@ if len(args) <= 1:
 user_prompt = args[1]
 messages = [types.Content(role = "user", parts=[types.Part(text = user_prompt)])]
 
-response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages)
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = types.Tool(function_declarations=[schema_get_files_info,])
+
+response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt))
 
 if "--verbose" in args:
     print(f"User prompt: {user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(response.text)
+if response.function_calls: 
+    response_functions = response.function_calls[0]
+    print(f"Calling function: {response_functions.name}({response_functions.args})")
+else:
+    print(response.text)
